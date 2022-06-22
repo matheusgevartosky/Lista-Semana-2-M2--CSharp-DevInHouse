d87e870ba45c9b31a865e88f7371c3dacf34e140
matheusgevartosky <magevartosky@gmail.com>
2022-06-22 12:37:47 -0300

    Finaliza Calculadora

--- Projeto_Calc.Console/Program.cs
+++ Projeto_Calc.Console/Program.cs
@@ -10,33 +10,40 @@
     }
     private static void Main(string[] args)
     {
-        Console.WriteLine("-----------------DevInCalculator----------------");
-        MostraMenu();
-        Console.WriteLine("-----------------------------------------------");
-
         bool condicao = true;
 
         var operation = new calculadora();
 
         while (condicao)
         {
+            Console.WriteLine("\n\n-----------------DevInCalculator----------------\n");
+            MostraMenu();
             Console.WriteLine("Digite a operação desejada:");
-            int operacao = int.Parse(Console.ReadLine());
+            var operacao = int.Parse(Console.ReadLine());
 
             var SelectedOp = (operacoes) operacao;
 
             switch (SelectedOp)
             {
                 case operacoes.soma:
-                Console.WriteLine("Digite o promeiro valor");
-                int value1 =int.Parse(Console.ReadLine());
-                Console.WriteLine("Digite o segundo valor");
-                int value2 =int.Parse(Console.ReadLine());
-                operation.SetValue1(value1, value2);
-                int result = operation.soma();
-                Console.WriteLine(result);
+                    MostraOpcao(1);
                 break;
-               
+                case operacoes.subtracao:
+                    MostraOpcao(2);
+                break;
+
+                case operacoes.multiplicacao:
+                    MostraOpcao(3);
+                break;
+                case operacoes.divisao:
+                    MostraOpcao(4);
+                break;
+                case operacoes.Sair:
+                    default:
+                    Console.WriteLine("Logout");
+                    Console.WriteLine("Obrigado, nos vemos em breve!");
+                    condicao = false;
+                break;
             }
 
         }
@@ -48,7 +55,39 @@
         Console.WriteLine("2 - Subtração");
         Console.WriteLine("3 - Multiplicação");
         Console.WriteLine("4 - Divisão");
-        Console.WriteLine("5 - Sair");
+        Console.WriteLine("5 - Sair\n");
+    }
+
+    public static void MostraOpcao(int op)
+    {
+        var operation = new calculadora();
+        Console.Write("Digite o primeiro valor:  ");
+            int value1 =int.Parse(Console.ReadLine());
+        Console.Write("Digite o segundo valor:  ");
+            int value2 =int.Parse(Console.ReadLine());
+        operation.SetValue1(value1, value2);
+        
+        if(op == 1)
+        {
+            int result = operation.soma();
+            Console.Write($"O Resultado é: {result}");
+        }
+        if(op == 2)
+        {
+            int result = operation.subtracao();
+            Console.WriteLine($"O Resultado é: {result}");
+        }
+        if(op == 3)
+        {
+            int result = operation.multiplicacao();
+            Console.WriteLine($"O Resultado é: {result}");
+        }
+        if(op == 4)
+        {
+            int result = operation.divisao();
+            Console.WriteLine($"O Resultado é: {result}");
+        }
+        
     }
     
 }
