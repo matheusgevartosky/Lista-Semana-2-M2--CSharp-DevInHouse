4443fe29e18ebac5c51b56c3b21aa3d797772a3d
matheusgevartosky <magevartosky@gmail.com>
2022-06-23 16:09:34 -0300

    Add BankAccount_projeto

---
+++ Projeto_BankAccount/Program.cs
@@ -0,0 +1,63 @@
+﻿namespace Projeto_BankAccount
+{
+    public class BankMenu
+    {
+        public enum OpcaoMenu { saldo = 1, extrato = 2, deposito = 3, saque = 4, sair = 5 }
+        static void Main(string[] args)
+        {
+            bool condicao = true;
+
+            var account = new bankAccount();
+
+            while (condicao)
+            {
+                MostraMenu();
+
+                var operacao = int.Parse(Console.ReadLine());
+                var SelectedOp = (OpcaoMenu)operacao;
+
+                switch (SelectedOp)
+                {
+                    case OpcaoMenu.saldo:
+                        var saldo = account.saldo;
+                        Console.WriteLine($"Seu salde é de R${saldo}");
+                        break;
+
+                    case OpcaoMenu.extrato:
+                        account.ImprimeExtrato();
+                        break;
+
+                    case OpcaoMenu.deposito:
+                        Console.Write("Insira o valor que será depositado: ");
+                        var deposito =double.Parse(Console.ReadLine());
+                        account.deposito(deposito);
+                        break;
+
+                    case OpcaoMenu.saque:
+                        Console.Write("Insira o valor que será sacado: ");
+                        var saque =double.Parse(Console.ReadLine());
+                        account.saque(saque);
+                        break;
+
+                    case OpcaoMenu.sair:
+                    default:
+                        Console.WriteLine("Logout");
+                        Console.WriteLine("Obrigado, nos vemos em breve!");
+                        condicao = false;
+                        break;
+                }
+            }
+
+            static void MostraMenu()
+            {
+                Console.WriteLine("\n----------------DevInBank-----------------\n");
+                Console.WriteLine("Digite a operação desejada:");
+                Console.WriteLine("1 - Saldo");
+                Console.WriteLine("2 - Extrato");
+                Console.WriteLine("3 - Deposito");
+                Console.WriteLine("4 - Saque");
+                Console.WriteLine("5 - Sair\n");
+            }
+        }
+    }
+}
---
+++ Projeto_BankAccount/conta.cs
@@ -0,0 +1,52 @@
+namespace Projeto_BankAccount
+{
+    class bankAccount
+    {
+        public double saldo { get; private set; }
+
+        private static List<string> extrato = new List<string>();
+
+
+        public void deposito(double deposito)
+        {
+            if(deposito <= 0)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                 Console.WriteLine("O valor depositádo não pode ser igual ou menos que 0");
+            }else
+            {
+                saldo += deposito;
+                extrato.Add($"Deposito efetuado.         R${deposito}");
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine($"Depósito de {deposito} efetuado com sucesso!");
+            }
+        }
+
+        public void saque(double saque)
+        {
+            if(saque > saldo)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                
+                Console.WriteLine($"não é possivel efetuar o saque, o valor solicitado é maior que o disponivel em conta. Saldo Atual: {saldo}");
+            }
+            else
+            {
+                saldo -= saque;
+                Console.ForegroundColor = ConsoleColor.Green;
+                extrato.Add($"Saque efetuado.             R${saque}");
+                Console.WriteLine($"Saque de {saque} efetuado. Saldo atual: {saldo}");
+            }
+        }
+
+        public void ImprimeExtrato()
+        {
+            Console.WriteLine("-----Extrato-----");
+            foreach (var item in extrato)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+    
+}
